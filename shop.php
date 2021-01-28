ca66429fe9aaa7b820149a48df53a0f7374fa8ab
Кирилл Волков <k.volkov.n@gmail.com>
2021-01-28 18:38:59 +0700

    Added good sorting

--- shop.php
+++ shop.php
@@ -3,14 +3,49 @@ session_start();
 
 require_once('auth/connection.php');
 
-$query = 'SELECT * FROM goods ORDER BY comments_count DESC;';
-$query_result = mysqli_query($connection, $query) 
-    or die("Ошибка " . mysqli_error($connection));
-if ($query_result) {
-    while ($data_array = mysqli_fetch_assoc($query_result)) {
-        $goods[] = $data_array;   
-    }
+//Сортировка товаров по дате, просмотрам и комментариям
+if ($_GET['sort_by']=='views') {
+  $sort_by = 'Просмотрам';
+  $query = 'SELECT * FROM goods ORDER BY views_count DESC;';
+  $query_result = mysqli_query($connection, $query) 
+      or die("Ошибка " . mysqli_error($connection));
+  if ($query_result) {
+      while ($data_array = mysqli_fetch_assoc($query_result)) {
+          $goods[] = $data_array;   
+      }
+  }
+}else if ($_GET['sort_by']=='comments'){
+   $sort_by = 'Комментариям';
+  $query = 'SELECT * FROM goods ORDER BY comments_count DESC;';
+  $query_result = mysqli_query($connection, $query) 
+      or die("Ошибка " . mysqli_error($connection));
+  if ($query_result) {
+      while ($data_array = mysqli_fetch_assoc($query_result)) {
+          $goods[] = $data_array;   
+      }
+  }
+}else if ($_GET['sort_by']=='date_desc'){
+  $sort_by = 'Дате(сначала старые)';
+  $query = 'SELECT * FROM goods ORDER BY id';
+  $query_result = mysqli_query($connection, $query) 
+      or die("Ошибка " . mysqli_error($connection));
+  if ($query_result) {
+      while ($data_array = mysqli_fetch_assoc($query_result)) {
+          $goods[] = $data_array;   
+      }
+  }
+}else {
+  $sort_by = 'Дате(сначала новые)';
+  $query = 'SELECT * FROM goods ORDER BY id DESC;';
+  $query_result = mysqli_query($connection, $query) 
+      or die("Ошибка " . mysqli_error($connection));
+  if ($query_result) {
+      while ($data_array = mysqli_fetch_assoc($query_result)) {
+          $goods[] = $data_array;   
+      }
+  }
 }
+
 
 
 //Если пользователь нажал "Подробнее" под товаром, то он переходит на эту же страницу, но в url передаётся id и, если товар с данным id есть в магазине, то подключается шаблон openedProduct.php с соотвествующим id товара, иначе попадаем в блок else, то есть выводится список всех товаров в магазине.
@@ -38,6 +73,18 @@ if ($good_found) {
 <?php require_once('templates/header.php'); ?>
 
 <h1>Каталог товаров</h1>
+
+<div class="dropdown">
+  <span style="font-size:24px;">Сортировать по:</span>
+  <button onclick="drop_down()" class="dropbtn"><?php echo $sort_by?></button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="http://brandshop/shop.php?sort_by=date">Дате(сначала новые)</a>
+    <a href="http://brandshop/shop.php?sort_by=date_desc">Дате(сначала старые)</a>
+    <a href="http://brandshop/shop.php?sort_by=views">Просмотрам</a>
+    <a href="http://brandshop/shop.php?sort_by=comments">Комментариям</a>
+  </div>
+</div>
+
 <div>
     <?php foreach ($goods as $good): ?>
         
--- templates/header.php
+++ templates/header.php
@@ -34,6 +34,7 @@ if(isset($_COOKIE["password_cookie_token"]) && !empty($_COOKIE["password_cookie_
     <meta charset="UTF-8">
     <script src="../scripts/jquery.js"></script>
     <script src="../scripts/site.js"></script>
+    <script src="../scripts/shop_drop-down_menu.js"></script>
     <title><?php echo $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="../styles/site.css" rel="stylesheet">
